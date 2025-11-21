4839965d51158f5f9889563510dc915db7d485fc
Elidrissi12 <zakiabdo122@gmail.com>
2025-11-21 12:32:27 +0100

    new

--- src/test/java/MetierImplTest.java
+++ src/test/java/MetierImplTest.java
@@ -8,7 +8,6 @@ public class MetierImplTest {
 
     @Test
     public void testCalcul() {
-        // Création d'une implémentation fictive de IDao pour le test
         IDao dao = new IDao() {
             @Override
             public double getValue() {
@@ -16,11 +15,9 @@ public class MetierImplTest {
             }
         };
 
-        // Création de l'instance de MetierImpl et injection manuelle de la dépendance
         MetierImpl metier = new MetierImpl();
         metier.setDao(dao);
 
-        // Vérification que le calcul donne le résultat attendu
         double result = metier.calcul();
         Assert.assertEquals(20.0, result, 0.001);  // 10.0 * 2 = 20.0
     }
